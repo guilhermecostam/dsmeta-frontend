6a850b21c93ad854219f3fc5be9853730bba9ed1
guilhermecostam <guicm007@gmail.com>
2022-07-16 17:26:47 -0300

    feat: add date picker lib

--- src/components/DatePickerForm/index.tsx
+++ src/components/DatePickerForm/index.tsx
@@ -1,13 +1,23 @@
 import './style.css'
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 function DatePickerForm() {
   return (
     <div>
         <div className="dsmeta-form-control">
-            <input type="text" />
+            <DatePicker
+                selected={new Date()}
+                onChange={(date: Date) => {}}
+                dateFormat="dd/MM/yyyy"
+            />
         </div>
         <div className="dsmeta-form-control">
-            <input type="text" />
+            <DatePicker
+                selected={new Date()}
+                onChange={(date: Date) => {}}
+                dateFormat="dd/MM/yyyy"
+            />
         </div>
     </div>
   )
