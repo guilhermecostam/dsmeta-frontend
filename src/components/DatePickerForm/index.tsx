8d29dd308de26e1c7f385ec1127fd3ec7f5bd0b1
guilhermecostam <guicm007@gmail.com>
2022-07-16 17:20:47 -0300

    feat: add date picker component component

---
+++ src/components/DatePickerForm/index.tsx
@@ -0,0 +1,16 @@
+import './style.css'
+
+function DatePickerForm() {
+  return (
+    <div>
+        <div className="dsmeta-form-control">
+            <input type="text" />
+        </div>
+        <div className="dsmeta-form-control">
+            <input type="text" />
+        </div>
+    </div>
+  )
+}
+
+export default DatePickerForm
