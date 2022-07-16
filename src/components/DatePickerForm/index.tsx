c60e47d1b12b5d2dce8677cadb73e175c56ae946
guilhermecostam <guicm007@gmail.com>
2022-07-16 17:53:20 -0300

    feat: add useState on date picker component

--- src/components/DatePickerForm/index.tsx
+++ src/components/DatePickerForm/index.tsx
@@ -1,26 +1,31 @@
 import './style.css'
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
+import DatePicker from "react-datepicker"
+import "react-datepicker/dist/react-datepicker.css"
+import { useState } from 'react'
 
 function DatePickerForm() {
-  return (
-    <div>
-        <div className="dsmeta-form-control">
-            <DatePicker
-                selected={new Date()}
-                onChange={(date: Date) => {}}
-                dateFormat="dd/MM/yyyy"
-            />
+    const today = new Date()
+    const [minDate, setMinDate] = useState(today)
+    const [maxDate, setMaxDate] = useState(today)
+
+    return (
+        <div>
+            <div className="dsmeta-form-control">
+                <DatePicker
+                    selected={minDate}
+                    onChange={(date: Date) => setMinDate(date)}
+                    dateFormat="dd/MM/yyyy"
+                />
+            </div>
+            <div className="dsmeta-form-control">
+                <DatePicker
+                    selected={maxDate}
+                    onChange={(date: Date) => setMaxDate(date)}
+                    dateFormat="dd/MM/yyyy"
+                />
+            </div>
         </div>
-        <div className="dsmeta-form-control">
-            <DatePicker
-                selected={new Date()}
-                onChange={(date: Date) => {}}
-                dateFormat="dd/MM/yyyy"
-            />
-        </div>
-    </div>
-  )
+    );
 }
 
 export default DatePickerForm
